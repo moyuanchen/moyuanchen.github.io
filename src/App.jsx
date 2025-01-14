8b60b99d245ed8ee9f210607d37472a1175821d3
moyuanchen <chenmoyuan1@gmail.com>
2025-01-14 11:31:01 +0000

    app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import BlackScholesCalculator from './BlackScholesCalculator.jsx';
+
+function App() {
+  return (
+    <div>
+      <BlackScholesCalculator />
+    </div>
+  );
+}
+
+export default App;
