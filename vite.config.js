4af2d721f0f2df1a87ab169402fd56aeddd282af
moyuanchen <chenmoyuan1@gmail.com>
2025-01-14 11:41:27 +0000

    build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+  base: '/moyuanchen.github.io/', // Add this line
   plugins: [react()],
   resolve: {
     alias: {
